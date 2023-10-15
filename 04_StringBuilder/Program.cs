2a03e22dc89e853c286a6921d6c4962d70760824
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-10-15 16:48:09 +0300

    Array, String

---
+++ 04_StringBuilder/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace _04_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";//5b
+            str += ", world!!!";//10b
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+            str += ", world!!!";
+
+            //str.Insert()
+            //char.IsUpper('A');
+            StringBuilder b = new StringBuilder();
+            b.AppendLine("bla");
+
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+
+            b.Append("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+            b.Append("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+            b.Append("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+            b.Append("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+            b.Append("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+            b.AppendLine("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+            b.Append("bla");
+            b.Append("bla");
+            b.Append("bla");
+            b.Append("bla");
+            b.Append("bla");
+            Console.WriteLine($" Length : {b.Length}");
+            Console.WriteLine($" Capacity : {b.Capacity}");
+
+            Console.WriteLine(b);
+
+
+
+
+        }
+    }
+}
